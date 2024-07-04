52a0c5dea21b1dd5f6249ee41cb09e6d60c280dd
pgyseven <pgyseven@naver.com>
2024-07-04 11:01:45 +0900

    feat:modelandview 객체 연습을 위한 exmodelandview() 생성

--- src/main/java/com/webpgy/controller/HomeController.java
+++ src/main/java/com/webpgy/controller/HomeController.java
@@ -2,7 +2,9 @@ package com.webpgy.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Handles requests for the application home page.
@@ -32,7 +35,7 @@ public class HomeController { //홈컨트롤클래스는 언제 객체화 되냐
 		
 		String formattedDate = dateFormat.format(date); //날짜 시간을 문자열로 바꾸고
 		
-		// Model : 뷰단으로 전송할 데이터를 깜쌀 전용 객체 즉 리퀘스트 필요없다 전용 객체가 있으니깐
+		// Model : 뷰단으로 전송할 데이터를 깜쌀 전용 객체다 그리고 리퀘스트 필요없다 전용 객체가 있으니깐
 		model.addAttribute("serverTime", formattedDate ); //Model 객체에 바인딩 view 단으로 전송하기 위해서 여기 forward 같은 것 / 바인딩(객체를 감싸서 전송하는 것) 시켰다.
 		
 		return "home"; //public String home(Locale locale, Model model) 여기서 스트링이니 문자로 보낼거고 포워딜될 뷰단의 주소가 home 이다.
@@ -57,7 +60,7 @@ public class HomeController { //홈컨트롤클래스는 언제 객체화 되냐
 	@RequestMapping ("/ex3")
 	public String examRedirect() {
 		System.out.println("examRedirect 메서드가 동작함 ....");
-		return "redirect:/";  // 다이렉는 페이지 이동이라는 뜻 이런방식은 로그인을 완료하는 경우 다시 그전으로 돌아가야 할때 
+		return "redirect:/";  // 다이렉는 페이지 이동이라는 뜻 이런방식은 로그인을 완료하는 경우 다시 그전으로 돌아가야 할때 사용할 수 있다.
 	}
 	
 	
@@ -69,6 +72,27 @@ public class HomeController { //홈컨트롤클래스는 언제 객체화 되냐
 		//포워딩 데이터를 전달하면서 뷰단 실행 리다이렉트는 재호출
 	}
 
-
+	@RequestMapping ("/ex5") //위의 model 객체와 같은 기능이다 아래와 같이 model view를 써도 안써도 되지만 이런것도 있다. 알아둬야 한다 현업에서는 누군가 이걸 사용하니깐
+	public ModelAndView exModelAndView() { //Ctrl 누르고 모델뷰위에 마우스를 올리고 API를 확인한다. (새창이 뜰거임) Open Implementation를 클릭하면 여러 메소드 중에 ModelAndView(String viewName, Map<String, ?> model) 이 메소드를 사용할 것 해당 메소드에서 map이라는 건, 키와 데이터의 쌍으로 관리하는게 mapp 데이터를 한번에 모아서 관리할 수 있는 컬렉션
+		
+		//ModelAndView 객체 : 출력될 view 단의 이름과, 출력될 데이터(model)을 가지고 있는 객체
+		
+		System.out.println("exModelAndView()...................");
+		
+		Map model = new HashMap<String, String>();
+		model.put("name", "dooly");
+		
+		/* 
+		 * 참고로 이렇게도 가능하다 위와 같은 방식이다.
+		 * ModelAndView m = new ModelAndView(); m.setViewName("ex5");
+		 * m.addObject("name","dooly");
+		 */
+		
+		
+		
+		return new ModelAndView("ex5", model);
+		
+		
+	}
 
 }
