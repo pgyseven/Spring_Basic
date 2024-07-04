2d4c98e4b7c7298ef83a961aae685f8b5ace7e84
pgyseven <pgyseven@naver.com>
2024-07-04 17:14:57 +0900

    feat: productVO 가 잘 수집되었다면, RedirectAttributes 객체에 msg 갑 바인딩

--- src/main/java/com/webpgy/controller/ExamController.java
+++ src/main/java/com/webpgy/controller/ExamController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.webpgy.model.ProductVO;
 
@@ -31,6 +32,7 @@ public class ExamController {
 		
 	}
 	
+	//requestParam을 이요한 매개변수 수집 방법
 	/*
 	 * @RequestMapping(value = "/inputProduct", method = RequestMethod.POST) public
 	 * void saveProduct(@RequestParam("productNo") String productoNo,
@@ -52,12 +54,26 @@ public class ExamController {
 	 * }
 	 */
 	
+	
+	// 파라메터를 Spring ftamwork가 자동 수집하여, 객체로 만들어준다.
 	@RequestMapping(value = "/inputProduct", method = RequestMethod.POST) 
-	public void saveProduct(ProductVO p) { //메소드의 매개 변수 자리! 이렇게 하면 기본 생성자를 호출해서 빈객체를 만들고 세터를 호출해서 넣어준다. / 객체로 만들고 싶을때는 이렇게 쓰고 아닐때는 위와 같이 쓴다. / 매개변수를 자동으로 수집해줌
-	System.out.println("saveProduct()..............!");
+	public String saveProduct(ProductVO p, RedirectAttributes redirectAttributes) { //메소드의 매개 변수 자리! 이렇게 하면 기본 생성자를 호출해서 빈객체를 만들고 세터를 호출해서 넣어준다. / 객체로 만들고 싶을때는 이렇게 쓰고 아닐때는 위와 같이 쓴다. / 매개변수를 자동으로 수집해줌
+	//RedirectAttributes 리다이렉트할때 가지고 갈수 있는 속성
+		System.out.println("saveProduct()..............!");
 	
 	// ProductVO p = new ProductVO(productoNo, productName, qty, price, productName);
 	System.out.println(p.toString());
+	
+	// p를 서비스단 -> dao 단 -> DB에 잘 저장했다 치자~~
+	String msg = null;
+	if(p != null) {
+		msg = "success";
+		
+	}
+	redirectAttributes.addAttribute("status",msg); //?status 쿼리 스트링 물음표 다음에 나오는게 쿼리 스트링 이렇게 하면 http://localhost:8081/controller/?status=success 리다이렉트에서 home의 주소에 이렇게 붙어있게 된다.
+	return "redirect:/"; //다른 컨트롤러 단으로 이동 가능하다 분리하라고 이렇게 한 것이다. 회원관리, 게시판 이런식으로 해당 기능의 컨트롤러가 있을거니깐 컨트롤러가 여러개 일 수 있고 호출이 가능하다.
+	
+	
 	
 	}
 	
